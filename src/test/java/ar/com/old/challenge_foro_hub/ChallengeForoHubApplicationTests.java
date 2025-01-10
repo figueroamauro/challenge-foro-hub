e030f744defa4efa2b03fd8cf5cb2c1e22e9b49b
mauro <figueroamauro.dev@gmail.com>
2025-01-10 14:13:52 -0300

    chore: add jpa configuration to application.properties -url, username, password, auto update, show sql.

---
+++ src/test/java/ar/com/old/challenge_foro_hub/ChallengeForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.old.challenge_foro_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
